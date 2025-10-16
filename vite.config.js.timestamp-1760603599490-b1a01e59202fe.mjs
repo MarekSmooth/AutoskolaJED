1f44159ad7a46a54ab103f13809cfeea2b7c1cd7
MarekSmooth <hladky.mary@gmail.com>
2025-10-16 10:33:24 +0200

    Added vite.config.js.timestamp-1760603599490-b1a01e59202fe.mjs

---
+++ vite.config.js.timestamp-1760603599490-b1a01e59202fe.mjs
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from "file:///home/project/node_modules/vite/dist/node/index.js";
+var vite_config_default = defineConfig({
+  base: "./",
+  // důležité pro GitHub Pages i Bolt
+  build: {
+    outDir: "dist",
+    assetsDir: "assets"
+  }
+});
+export {
+  vite_config_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsidml0ZS5jb25maWcuanMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbImNvbnN0IF9fdml0ZV9pbmplY3RlZF9vcmlnaW5hbF9kaXJuYW1lID0gXCIvaG9tZS9wcm9qZWN0XCI7Y29uc3QgX192aXRlX2luamVjdGVkX29yaWdpbmFsX2ZpbGVuYW1lID0gXCIvaG9tZS9wcm9qZWN0L3ZpdGUuY29uZmlnLmpzXCI7Y29uc3QgX192aXRlX2luamVjdGVkX29yaWdpbmFsX2ltcG9ydF9tZXRhX3VybCA9IFwiZmlsZTovLy9ob21lL3Byb2plY3Qvdml0ZS5jb25maWcuanNcIjtpbXBvcnQgeyBkZWZpbmVDb25maWcgfSBmcm9tICd2aXRlJztcblxuXG5leHBvcnQgZGVmYXVsdCBkZWZpbmVDb25maWcoe1xuYmFzZTogJy4vJywgLy8gZFx1MDE2RmxlXHUwMTdFaXRcdTAwRTkgcHJvIEdpdEh1YiBQYWdlcyBpIEJvbHRcbmJ1aWxkOiB7XG5vdXREaXI6ICdkaXN0JyxcbmFzc2V0c0RpcjogJ2Fzc2V0cydcbn1cbn0pO1xuIl0sCiAgIm1hcHBpbmdzIjogIjtBQUF5TixTQUFTLG9CQUFvQjtBQUd0UCxJQUFPLHNCQUFRLGFBQWE7QUFBQSxFQUM1QixNQUFNO0FBQUE7QUFBQSxFQUNOLE9BQU87QUFBQSxJQUNQLFFBQVE7QUFBQSxJQUNSLFdBQVc7QUFBQSxFQUNYO0FBQ0EsQ0FBQzsiLAogICJuYW1lcyI6IFtdCn0K
